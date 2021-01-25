c32897826238ff3afbf4ed82313966af47a46d27
AK-Devil <abdulkhaliql1f18bscs0159@gmail.com>
2021-01-25 11:20:03 +0500

    add method to check newline before open bracket

--- lib/styl_lint.rb
+++ lib/styl_lint.rb
@@ -13,4 +13,16 @@ class Styllint
       # p 'okay fine' if sp[index] == ' '
     end
   end
+
+  def check_newline_after_open_bracket(sp, index_string)
+    s = StringScanner.new(sp)
+    flag = s.scan_until(/\{+/)
+    if flag.nil?
+      # p 'okay fine'
+    else
+      index = s.pos
+      p "line:#{index_string}|#{index}: must be a newline after {" unless sp[index] == "\n"
+      # p 'okay fine' if sp[index] == "\n"
+    end
+  end
 end
