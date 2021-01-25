dd9f5102b75529486cb34ec9d4da0778d1fe96cd
AK-Devil <abdulkhaliql1f18bscs0159@gmail.com>
2021-01-25 12:24:50 +0500

    Fix all methods

--- lib/styl_lint.rb
+++ lib/styl_lint.rb
@@ -23,17 +23,14 @@ class Styllint
   def check_intentation_for_regular_line(line, index_string, error)
     s = StringScanner.new(line)
     flag = s.scan_until(/\;+/)
-    if flag.nil?
-      # p 'okay fine'
-    else
+    unless flag.nil?
       temp = StringScanner.new(line)
       flag1 = temp.scan_until(/\{+/)
       unless flag1.nil?
         index = temp.pos
-        p "line:#{index_string}|#{index}: must be a newline after {" unless line[index] == "\n"
+        error << "line:#{index_string}|#{index}: must be a newline after {" unless line[index] == "\n"
       else
-        # p 'okay fine' if sp[0] == ' ' && sp[1] == ' '
-        p "line:#{index_string}|1: must be two space start" unless line[0] == ' ' && line[1] == ' '
+        error << "line:#{index_string}|1: must be two space start" unless line[0] == ' ' && line[1] == ' '
       end
     end
   end
@@ -41,14 +38,11 @@ class Styllint
   def check_space_after_colon(line, index_string, error)
     s = StringScanner.new(line)
     flag = s.scan_until(/\:+/)
-    if flag.nil?
-      # p 'okay fine'
-    else
+    unless flag.nil?
       index = s.pos
-      p "line:#{index_string}|#{index}: must be a space after colon " unless line[index] == ' '
-      if sp[index] == ' '
-        p "line:#{index_string}|#{index}: must be one space after colon " if line[index + 1] == ' '
-        # p 'okay fine' unless sp[index + 1] == ' '
+      error << "line:#{index_string}|#{index}: must be a space after colon " unless line[index] == ' '
+      if line[index] == ' '
+        error << "line:#{index_string}|#{index}: must be one space after colon " if line[index + 1] == ' '
       end
     end
   end
@@ -56,35 +50,29 @@ class Styllint
   def check_newline_after_semicolon(line, index_string, error)
     s = StringScanner.new(line)
     flag = s.scan_until(/\;+/)
-    if flag.nil?
-      # p 'okay fine'
-    else
+    unless flag.nil?
       index = s.pos
-      if sp[index] == ' '
-        p "line:#{index_string}|#{index}: there is a white space after semicolon"
+      if line[index] == ' '
+        error << "line:#{index_string}|#{index}: there is a white space after semicolon"
       else
-        p "line:#{index_string}|#{index}: must be a newline after semicolon" unless line[index] == "\n"
-        # p 'pksy fine' if sp[index] == "\n"
+        error << "line:#{index_string}|#{index}: must be a newline after semicolon" unless line[index] == "\n"
       end
-      p "line:#{index_string}|#{index}: there is a white space before semicolon" if line[index - 2] == ' '
+      error << "line:#{index_string}|#{index}: there is a white space before semicolon" if line[index - 2] == ' '
     end
   end
 
   def check_newline_after_closing_bracket(line, index_string, error)
     s = StringScanner.new(line)
     flag = s.scan_until(/\}/)
-    if flag.nil?
-      # p 'okay fine'
-    else
+    unless flag.nil?
       index = s.pos
-      p "line:#{index_string}|#{index}: must be a newline after closing bracket" unless line[index] == "\n"
-      # p 'okay fine' if sp[index] == "\n"
+      error << "line:#{index_string}|#{index}: must be a newline after closing bracket" unless line[index] == "\n"
     end
   end
 
   def check_case_sentitive(line, index_string, error)
     capital_count = line.scan(/[A-Z]/).length
-    p "line:#{index_string}: must be a small letter" unless capital_count <= 0
+    error << "line:#{index_string}: must be a small letter" unless capital_count <= 0
   end
 
   def check_error(error)
@@ -93,7 +81,7 @@ class Styllint
       check_space_before_open_bracket(i, index, error)
       check_newline_after_open_bracket(i, index, error)
       check_intentation_for_regular_line(i, index, error)
-      check_space_after_colon(i, index+1, error)
+      check_space_after_colon(i, index, error)
       check_newline_after_semicolon(i, index, error)
       check_newline_after_closing_bracket(i, index, error)
       check_case_sentitive(i, index, error)
