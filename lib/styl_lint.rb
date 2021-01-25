20ddf8c8957d12b5ee7787edab1a4665e6a5e939
AK-Devil <abdulkhaliql1f18bscs0159@gmail.com>
2021-01-25 11:21:49 +0500

    add method to check space after colon

--- lib/styl_lint.rb
+++ lib/styl_lint.rb
@@ -43,4 +43,19 @@ class Styllint
       end
     end
   end
+
+  def check_space_after_colon(sp, index_string)
+    s = StringScanner.new(sp)
+    flag = s.scan_until(/\:+/)
+    if flag.nil?
+      # p 'okay fine'
+    else
+      index = s.pos
+      p "line:#{index_string}|#{index}: must be a space after colon " unless sp[index] == ' '
+      if sp[index] == ' '
+        p "line:#{index_string}|#{index}: must be one space after colon " if sp[index + 1] == ' '
+        # p 'okay fine' unless sp[index + 1] == ' '
+      end
+    end
+  end
 end
