abbbb671bf2b4da3685008444a1df73ede8b7ef1
AK-Devil <abdulkhaliql1f18bscs0159@gmail.com>
2021-01-26 06:21:23 +0500

    Add private and public for methods

--- spec/styl_lint_spec.rb
+++ spec/styl_lint_spec.rb
@@ -8,13 +8,14 @@ describe Styllint do
     it 'must be space before open bracket' do
       error = []
       line = '.div{'
-      expect(instance_variable.check_space_before_open_bracket(line, 1, error)).to eql(true)
+      result = instance_variable.send(:check_space_before_open_bracket, line, 1, error)
+      expect(result).to eql(true)
     end
 
     it 'there is space before open bracket' do
       error = []
       line = '.div {'
-      expect(instance_variable.check_space_before_open_bracket(line, 1, error)).to eql(false)
+      expect(instance_variable.send(:check_space_before_open_bracket, line, 1, error)).to eql(false)
     end
   end
 
@@ -22,13 +23,13 @@ describe Styllint do
     it 'must be a newline after open bracket' do
       error = []
       line = '.div {'
-      expect(instance_variable.check_newline_after_open_bracket(line, 1, error)).to eql(true)
+      expect(instance_variable.send(:check_newline_after_open_bracket, line, 1, error)).to eql(true)
     end
 
     it 'there is a newline after open bracket' do
       error = []
       line = ".div {\n"
-      expect(instance_variable.check_newline_after_open_bracket(line, 1, error)).to eql(false)
+      expect(instance_variable.send(:check_newline_after_open_bracket, line, 1, error)).to eql(false)
     end
   end
 
@@ -36,25 +37,25 @@ describe Styllint do
     it 'must be intentation for regular line' do
       error = []
       line = 'float:left;'
-      expect(instance_variable.check_intentation_for_regular_line(line, 1, error)).to eql(true)
+      expect(instance_variable.send(:check_intentation_for_regular_line, line, 1, error)).to eql(true)
     end
 
     it 'must be a newline after open bracket' do
       error = []
       line = '.div{float:left;'
-      expect(instance_variable.check_intentation_for_regular_line(line, 1, error)).to eql(true)
+      expect(instance_variable.send(:check_intentation_for_regular_line, line, 1, error)).to eql(true)
     end
 
     it 'there is intentation for regular line' do
       error = []
       line = '  float:left;'
-      expect(instance_variable.check_intentation_for_regular_line(line, 1, error)).to eql(false)
+      expect(instance_variable.send(:check_intentation_for_regular_line, line, 1, error)).to eql(false)
     end
 
     it 'must be a newline after open bracket and intentation for regular line' do
       error = []
       line = ".div {\n  float:left;"
-      expect(instance_variable.check_intentation_for_regular_line(line, 1, error)).to eql(false)
+      expect(instance_variable.send(:check_intentation_for_regular_line, line, 1, error)).to eql(false)
     end
   end
 
@@ -62,19 +63,19 @@ describe Styllint do
     it 'must be a space after colon' do
       error = []
       line = 'float:left;'
-      expect(instance_variable.check_space_after_colon(line, 1, error)).to eql(true)
+      expect(instance_variable.send(:check_space_after_colon, line, 1, error)).to eql(true)
     end
 
     it 'there is double space after colon' do
       error = []
       line = 'float:  left;'
-      expect(instance_variable.check_space_after_colon(line, 1, error)).to eql(true)
+      expect(instance_variable.send(:check_space_after_colon, line, 1, error)).to eql(true)
     end
 
     it 'there is a space after colon' do
       error = []
       line = 'float: left;'
-      expect(instance_variable.check_space_after_colon(line, 1, error)).to eql(false)
+      expect(instance_variable.send(:check_space_after_colon, line, 1, error)).to eql(false)
     end
   end
 
@@ -82,25 +83,25 @@ describe Styllint do
     it 'there is a space after semicolon' do
       error = []
       line = 'float:left; '
-      expect(instance_variable.check_newline_after_semicolon(line, 1, error)).to eql(true)
+      expect(instance_variable.send(:check_newline_after_semicolon, line, 1, error)).to eql(true)
     end
 
     it 'there is a space before semicolon' do
       error = []
       line = 'float:left ;'
-      expect(instance_variable.check_newline_after_semicolon(line, 1, error)).to eql(true)
+      expect(instance_variable.send(:check_newline_after_semicolon, line, 1, error)).to eql(true)
     end
 
     it 'there is no newline after semicolon' do
       error = []
       line = 'float:left;'
-      expect(instance_variable.check_newline_after_semicolon(line, 1, error)).to eql(true)
+      expect(instance_variable.send(:check_newline_after_semicolon, line, 1, error)).to eql(true)
     end
 
     it 'there is a newline after semicolon' do
       error = []
       line = "float:left;\n"
-      expect(instance_variable.check_newline_after_semicolon(line, 1, error)).to eql(false)
+      expect(instance_variable.send(:check_newline_after_semicolon, line, 1, error)).to eql(false)
     end
   end
 
@@ -108,13 +109,13 @@ describe Styllint do
     it 'there is no newline after closing bracket' do
       error = []
       line = '}'
-      expect(instance_variable.check_newline_after_closing_bracket(line, 1, error)).to eql(true)
+      expect(instance_variable.send(:check_newline_after_closing_bracket, line, 1, error)).to eql(true)
     end
 
     it 'there is a newline after closing bracket' do
       error = []
       line = "}\n"
-      expect(instance_variable.check_newline_after_closing_bracket(line, 1, error)).to eql(false)
+      expect(instance_variable.send(:check_newline_after_closing_bracket, line, 1, error)).to eql(false)
     end
   end
 
@@ -122,13 +123,13 @@ describe Styllint do
     it 'would return count of capital letter' do
       error = []
       line = 'FloAt:left;'
-      expect(instance_variable.check_case_sentitive(line, 1, error)).to eql(2)
+      expect(instance_variable.send(:check_case_sentitive, line, 1, error)).to eql(2)
     end
 
     it 'would return zero if there is no count of capital letter' do
       error = []
       line = 'float:left;'
-      expect(instance_variable.check_case_sentitive(line, 1, error)).to eql(0)
+      expect(instance_variable.send(:check_case_sentitive, line, 1, error)).to eql(0)
     end
   end
 end
